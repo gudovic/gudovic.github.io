5787b39958a18c6022c89d8932bab235b4469919
maxmax <174373891+gudovic@users.noreply.github.com>
2025-04-26 22:49:29 +0200

    Massa nya grejer

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const validCodes = {
-    '1234': { userName: 'Max', group: 'group1.html' },
-    '5678': { userName: 'Bullen', group: 'group2.html' }
+    '1234': { userName: 'Max Mattiasson', userGroup: 'group1' },
+    '5678': { userName: 'Bullen', userGroup: 'group2' }
 }
 
 const codeInput = document.getElementById('codeInput');
@@ -12,9 +12,19 @@ function submitCode() {
 
     if (userData) {
         localStorage.setItem('userName', userData.userName);
-        localStorage.setItem('groupPage', userData.group);
+        localStorage.setItem('userGroup', userData.userGroup);
         window.location.href = 'welcome.html';
     } else {
         errorElement.textContent = 'Fel bokningsnummer, vänligen försök igen.';
     }
 }
+
+const userName = localStorage.getItem('userName');
+
+    if (userName) {
+        welcomeMessage.textContent = `Välkommen ${userName}!`;
+    }
+
+    function goToNextPage() {
+        window.location.href = 'destination.html';
+    }
