4c9e9ccd5e8fde148f07ca1899aa092c2c41d5b8
maxmax <174373891+gudovic@users.noreply.github.com>
2025-05-22 23:58:49 +0200

    temporärt för en dag, fixar sen

---
+++ config.js
@@ -0,0 +1,11 @@
+const FIREBASE_API_KEY = "AIzaSyD0Y3nAUUwQUT5cwFEEDaG3sGDMcnnoJyo";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0Y3nAUUwQUT5cwFEEDaG3sGDMcnnoJyo",
+  authDomain: "lf-airlines.firebaseapp.com",
+  databaseURL: "https://lf-airlines-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lf-airlines",
+  storageBucket: "lf-airlines.appspot.com",
+  messagingSenderId: "719527670719",
+  appId: "1:719527670719:web:a3cdc391b0858a8a72a70a"
+};
